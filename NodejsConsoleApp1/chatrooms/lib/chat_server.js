8d1816c5dc329cd44f5611a87fdf515f10aaee2d
hesterdev <940356013@qq.com>
2016-03-23 23:44:29 +0800

    要参加考研复试了,忙里偷闲来一段

--- NodejsConsoleApp1/chatrooms/lib/chat_server.js
+++ NodejsConsoleApp1/chatrooms/lib/chat_server.js
@@ -1,7 +1,7 @@
 ﻿var socketio = require('socket.io');
 var io;
 var guestNumber = 1;
-var nickName = {};
+var nickNames = {};
 var namesUsed = [];
 var currentRoom = {};
 
@@ -18,7 +18,7 @@ exports.listen = function (server) {
 		
 		//在用户连接上来时把他放入聊天室Lobby里
 		joinRoom(socket, 'Lobby');
-
+		
 		handleMessageBroadcasting(socket, nickName);
 		//处理用户的消息更名,以及聊天室的创建和变更
 		handleNameChangeAttempts(socket, nickName, namesUsed);
@@ -33,3 +33,76 @@ exports.listen = function (server) {
 		handleClientDisconnection(socket, nickName, namesUsed);
 	});
 };
+function assignGuestName(socket, guestNumber, nickName, namesUsed) {
+	// 生成新昵称
+	var name = 'Guest' + guestNumber;
+	//把用户昵称跟客户端连接ID关联上
+	nickNames[socket.id] = name;
+	
+	//让用户知道他们的昵称
+	socket.emit('nameResult', {
+		success: true,
+		name: name
+	});
+	namesUsed.push(name);//存放已经被占用的昵称
+	return guestNumber + 1;//增加用来生成昵称的计数器
+}
+
+function joinRoom(socket, room) {
+	socket.join(room);//让用户进入房间
+	currentRoom[socket.id] = room;//记录用户的当前房间
+	socket.emit('joinResult', { room: room });
+	
+	//让房间里的其他用户知道有新用户进入了房间
+	socket.broadcast.to(room).emit('message', {
+		text: nickNames[socket.id] + ' has joined ' + room + '.'
+	});
+	
+	var usersInRoom = io.sockets.clients(room);
+	if (usersInRoom.length > 1) {
+		var usersInRoomSummary = 'Users current in ' + room + ': ';
+		for (var index in usersInRoom) {
+			var userSocketId = usersInRoom[index].id;
+			if (userSocketId != socket.id) {
+				if (index > 0) {
+					usersInRoomSummary += ', ';
+				}
+				usersInRoomSummary += nickNames[userSocketId];
+			}
+		}
+		usersInRoomSummary += '.';
+		//将房间里其他用户汇总发送给这个用户
+		socket.emit('message', { text: usersInRoomSummary });
+	}
+}
+
+// 更名请求的处理逻辑
+function handleNameChangeAttempts(socket, nickNames, namesUsed) {
+	socket.on('nameAttempt', function (name) {
+		if (name.indexOf('Guest') == 0) {
+			socket.emit('nameResult', {
+				success: false,
+				message: 'Names cannot begin with "Guest".'
+			});
+		} else {
+			if (namesUsed.indexOf(name) == -1) {
+				var previousName = nickNames[socket.id];
+				var previousNameIndex = namesUsed.indexOf(previousName);
+				namesUsed[socket.id] = name;
+				delete namesUsed[previousNameIndex];//删掉之前的昵称,让其他用户可以使用
+				socket.emit('nameResult', {
+					success: true,
+					name: name
+				});
+				socket.broadcast.to(currentRoom[socket.id]).emit('message', {
+					text: previousName + ' is now known as ' + name + '.'
+				});
+			} else {
+				socket.emit('nameResult', {
+					success: false,
+					message: 'That naem is already in use.'
+				});
+			}
+		}
+	});
+}
